9eac6c2e330a5b4cd47e892a8fe553517958ed24
Abhijit <abhijitkghosh29@gmail.com>
2017-09-23 01:32:46 -0400

    updatng without console logs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,16 +14,16 @@ function numbergenerator(min,max){
 
 $(document).ready(function(){
 targetScore=numbergenerator(19,120);
-console.log(targetScore);
+//console.log(targetScore);
 $('#TargetScore').text(targetScore);
 ruby=numbergenerator(1,12);
-console.log("RUby"+ruby);
+//console.log("RUby"+ruby);
 emrald=numbergenerator(1,12);
-console.log("Emarald"+emrald);
+//console.log("Emarald"+emrald);
 topaz=numbergenerator(1,12);
-console.log("Topaz"+topaz);
+//console.log("Topaz"+topaz);
 sapphire=numbergenerator(1,12);
-console.log("Sapphire"+sapphire);
+//console.log("Sapphire"+sapphire);
 
 });
 
@@ -31,16 +31,16 @@ console.log("Sapphire"+sapphire);
 
 function reset(){
 targetScore=numbergenerator(19,120);
-console.log(targetScore);
+//console.log(targetScore);
 $('#TargetScore').text(targetScore);
 ruby=numbergenerator(1,12);
-console.log("RUby"+ruby);
+//console.log("RUby"+ruby);
 emrald=numbergenerator(1,12);
-console.log("Emarald"+emrald);
+//console.log("Emarald"+emrald);
 topaz=numbergenerator(1,12);
-console.log("Topaz"+topaz);
+//console.log("Topaz"+topaz);
 sapphire=numbergenerator(1,12);
-console.log("Sapphire"+sapphire);
+//console.log("Sapphire"+sapphire);
 additiveScore=0;
 $('#additiveScore').text(additiveScore);
 };
@@ -48,8 +48,8 @@ $('#additiveScore').text(additiveScore);
 $(document).ready(function(){
 $("#ruby").click(function(){
 		additiveScore=additiveScore+ruby;
-		console.log("RUby"+ruby);
-		console.log(additiveScore);
+		//console.log("RUby"+ruby);
+		//console.log(additiveScore);
 		$('#additiveScore').text(additiveScore);
 		decide();
 });
@@ -58,8 +58,8 @@ $("#ruby").click(function(){
 $(document).ready(function(){
 $("#sapphire").click(function(){
 		additiveScore=additiveScore+sapphire;
-		console.log("Sapphire"+sapphire);
-		console.log(additiveScore);
+		//console.log("Sapphire"+sapphire);
+		//console.log(additiveScore);
 		$('#additiveScore').text(additiveScore);
 		decide();
 });
@@ -68,8 +68,8 @@ $("#sapphire").click(function(){
 $(document).ready(function(){
 $("#topaz").click(function(){
 		additiveScore=additiveScore+topaz;
-		console.log("Topaz"+topaz);
-		console.log(additiveScore);
+		//console.log("Topaz"+topaz);
+		//console.log(additiveScore);
 		$('#additiveScore').text(additiveScore);
 		decide();
 });
@@ -78,8 +78,8 @@ $("#topaz").click(function(){
 $(document).ready(function(){
 $("#emrald").click(function(){
 		additiveScore=additiveScore+emrald;
-		console.log("Emarald"+emrald);
-		console.log(additiveScore);
+		//console.log("Emarald"+emrald);
+		//console.log(additiveScore);
 		$('#additiveScore').text(additiveScore);
 		decide();
 });
@@ -93,13 +93,13 @@ $("#emrald").click(function(){
 function decide(){
 	if (additiveScore === targetScore ){
 	wins++;
-	console.log("wins " + wins);
+	//console.log("wins " + wins);
 	$('#Wins').text(wins);
 	reset();
 }
  else if(additiveScore > targetScore){
  	losses++;
- 	console.log("losses " + losses);
+ 	//console.log("losses " + losses);
  	$('#Losses').text(losses);
  	reset();
  }
