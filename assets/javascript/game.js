5e407ab0c8b4a310adef35b106a492802fd6aa51
Abhijit <abhijitkghosh29@gmail.com>
2017-09-23 01:14:52 -0400

    uploading final touches

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,8 +11,11 @@ function numbergenerator(min,max){
 	return Math.floor(Math.random()*(max-min+1)+min);
 }
 
+
+$(document).ready(function(){
 targetScore=numbergenerator(19,120);
 console.log(targetScore);
+$('#TargetScore').text(targetScore);
 ruby=numbergenerator(1,12);
 console.log("RUby"+ruby);
 emrald=numbergenerator(1,12);
@@ -22,9 +25,14 @@ console.log("Topaz"+topaz);
 sapphire=numbergenerator(1,12);
 console.log("Sapphire"+sapphire);
 
+});
+
+
+
 function reset(){
 targetScore=numbergenerator(19,120);
 console.log(targetScore);
+$('#TargetScore').text(targetScore);
 ruby=numbergenerator(1,12);
 console.log("RUby"+ruby);
 emrald=numbergenerator(1,12);
@@ -33,6 +41,8 @@ topaz=numbergenerator(1,12);
 console.log("Topaz"+topaz);
 sapphire=numbergenerator(1,12);
 console.log("Sapphire"+sapphire);
+additiveScore=0;
+$('#additiveScore').text(additiveScore);
 };
 
 $(document).ready(function(){
@@ -40,6 +50,7 @@ $("#ruby").click(function(){
 		additiveScore=additiveScore+ruby;
 		console.log("RUby"+ruby);
 		console.log(additiveScore);
+		$('#additiveScore').text(additiveScore);
 		decide();
 });
 });
@@ -49,6 +60,7 @@ $("#sapphire").click(function(){
 		additiveScore=additiveScore+sapphire;
 		console.log("Sapphire"+sapphire);
 		console.log(additiveScore);
+		$('#additiveScore').text(additiveScore);
 		decide();
 });
 });
@@ -58,6 +70,7 @@ $("#topaz").click(function(){
 		additiveScore=additiveScore+topaz;
 		console.log("Topaz"+topaz);
 		console.log(additiveScore);
+		$('#additiveScore').text(additiveScore);
 		decide();
 });
 });
@@ -67,19 +80,27 @@ $("#emrald").click(function(){
 		additiveScore=additiveScore+emrald;
 		console.log("Emarald"+emrald);
 		console.log(additiveScore);
+		$('#additiveScore').text(additiveScore);
 		decide();
 });
 });
+
+//$('#TargetScore').append(targetScore);
+//$('#additiveScore').append(additiveScore);
+
+
 
 function decide(){
 	if (additiveScore === targetScore ){
 	wins++;
 	console.log("wins " + wins);
+	$('#Wins').text(wins);
 	reset();
 }
  else if(additiveScore > targetScore){
  	losses++;
  	console.log("losses " + losses);
+ 	$('#Losses').text(losses);
  	reset();
  }
 }
